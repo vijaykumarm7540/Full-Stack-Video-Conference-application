4be8bd752ac17bd9abea5535d8120f4c651bed37
vijaykumarm <vijaykumarm6959@gmail.com>
2026-08-12 15:44:16 +0530

    first

---
+++ zoom-main/backend/src/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { createServer } from "node:http";
+import dotenv from "dotenv";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+dotenv.config();
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
+    try {
+        app.set("mongo_user")
+        const connectionDb = await mongoose.connect(process.env.MONGO_URI)
+
+        console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`)
+        server.listen(app.get("port"), () => {
+            console.log(`LISTENING ON PORT ${app.get("port")}`)
+        });
+    } catch (error) {
+        console.error("MongoDB Connection Error:", error.message)
+        process.exit(1)
+    }
+}
+
+start();
